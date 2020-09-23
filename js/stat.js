eb4a9875ea6ecdcbc0ba7e91f1948945dc39f58c
Lev Karpov <nt66@bk.ru>
2020-09-23 21:23:45 +0300

    Изменяет способ нахождение максимального значения массива. Уменьшает количество данных в фомуле, заменяя их константами

--- js/stat.js
+++ js/stat.js
@@ -7,30 +7,31 @@ const GAP = 10;
 const GAP_COLUMNS = 50;
 const BAR_WIDTH = 40;
 const BAR_HEIGHT = 150;
+const BAR_INDENT = BAR_WIDTH + GAP_COLUMNS;
 const statisticCoordinateX = CLOUD_X + GAP * 3;
 const statisticCoordinateY = CLOUD_HEIGHT - GAP * 2;
 const statisticTextCoordinateY = statisticCoordinateY - GAP * 3;
-
+const colorEnemy = `hsl(240,` + Math.floor(Math.random() * 100) + `%, 50%)`;
 
 const renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-const getMaxElement = function (arr) {
+/* const getMaxElement = function (arr) {
   let maxElement = arr[0];
   for (let i = 1; i < arr.length; i++) {
     if (arr[i] > maxElement) {
       maxElement = arr[i];
     }
   } return maxElement;
-};
+};*/
 
 window.renderStatistics = function (ctx, players, times) {
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, `rgba(0, 0, 0, 0.7)`);
   renderCloud(ctx, CLOUD_X, CLOUD_Y, `#fff`);
 
-  const maxTime = getMaxElement(times);
+  const maxTime = Math.floor(Math.max(...times)); // находит максимальный элемент массива
 
   ctx.fillStyle = `#000`;
   ctx.font = `16px PT Mono`;
@@ -41,9 +42,10 @@ window.renderStatistics = function (ctx, players, times) {
 
   for (let i = 0; i < players.length; i++) {
     ctx.fillStyle = `#000`;
-    ctx.fillText(players[i], statisticCoordinateX + (BAR_WIDTH + GAP_COLUMNS) * i, statisticCoordinateY);
-    ctx.fillText(Math.floor(times[i]), statisticCoordinateX + (BAR_WIDTH + GAP_COLUMNS) * i, statisticTextCoordinateY - (BAR_HEIGHT * times[i]) / maxTime);
-    ctx.fillStyle = players[i] === `Вы` ? `rgba(255, 0, 0, 1)` : `hsl(240,` + Math.floor(Math.random() * 100) + `%, 50%)`;
-    ctx.fillRect(statisticCoordinateX + (BAR_WIDTH + GAP_COLUMNS) * i, statisticCoordinateY - GAP - ((BAR_HEIGHT * times[i]) / maxTime), BAR_WIDTH, (BAR_HEIGHT * times[i]) / maxTime);
+    ctx.fillStyle = `#000`;
+    ctx.fillText(players[i], statisticCoordinateX + BAR_INDENT * i, statisticCoordinateY);
+    ctx.fillText(Math.floor(times[i]), statisticCoordinateX + BAR_INDENT * i, statisticTextCoordinateY - (BAR_HEIGHT * times[i]) / maxTime);
+    ctx.fillStyle = players[i] === `Вы` ? `rgba(255, 0, 0, 1)` : colorEnemy;
+    ctx.fillRect(statisticCoordinateX + BAR_INDENT * i, statisticCoordinateY - GAP - ((BAR_HEIGHT * times[i]) / maxTime), BAR_WIDTH, (BAR_HEIGHT * times[i]) / maxTime);
   }
 };
